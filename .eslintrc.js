1442623a6dc3b742edd869f464b970601591aff1
takahashikosuke <takohashi0410@gmail.com>
2022-10-21 00:52:26 +0900

    :sparkles: add story book

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    "plugin:react/recommended",
-    "standard-with-typescript",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
+  extends: ["plugin:react/recommended", "standard-with-typescript", "plugin:react-hooks/recommended", "prettier", "plugin:storybook/recommended"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    project: ["./tsconfig.json"],
+    project: ["./tsconfig.json"]
   },
   plugins: ["react"],
   rules: {
@@ -21,11 +16,11 @@ module.exports = {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
     // MEMO: https://github.com/react-hook-form/react-hook-form/discussions/8020
-    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/no-misused-promises": "off"
   },
   settings: {
     react: {
-      version: "detect",
-    },
-  },
+      version: "detect"
+    }
+  }
 };
