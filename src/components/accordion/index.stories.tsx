7b14039c898e6e1cec4e8bc42be530af9de0247c
takahashikosuke <takahashikosuke@team-lab.com>
2022-11-16 23:38:44 +0900

    :wrench: 修正

--- src/components/accordion/index.stories.tsx
+++ src/components/accordion/index.stories.tsx
@@ -7,9 +7,9 @@ export default {
   component: Accordion,
 } as ComponentMeta<typeof Accordion>;
 
-export const Accordion: ComponentStoryObj<typeof Accordion> = {
+export const AccordionStory: ComponentStoryObj<typeof Accordion> = {
   args: {
       contents: [{ id: 0, label: 'test1', text: 'test text' },{ id: 1, label: 'test2', text: 'test text' }]
   },
-  render: ({ title }) => <TestComponent title={title} />
+  render: ({ contents }) => <Accordion contents={contents} />
 }
