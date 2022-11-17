c66805bcb65026fd1f24e10a3c82fbd4bb625355
takahashikosuke <takahashikosuke@team-lab.com>
2022-11-17 23:23:32 +0900

    :bug: ファイル指定ミス

--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "./index.module.css";
+import styles from "./index.module.scss";
 
 interface Props {
   contents: Array<{ id: number; label: string; text: string }>;
