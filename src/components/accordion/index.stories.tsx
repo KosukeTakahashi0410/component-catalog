587f238df3bc0605a2284a3547e57922a393f580
takahashikosuke <takahashikosuke@team-lab.com>
2023-03-01 13:28:33 +0900

    :construction: toBeInTheDocumentが正しく稼働するかを見てみた

--- src/components/accordion/index.stories.tsx
+++ src/components/accordion/index.stories.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 
 import { Accordion } from './index';
 
+import { within } from '@storybook/testing-library';
+
+import { expect } from '@storybook/jest';
+
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 export default {
   component: Accordion,
@@ -13,4 +17,10 @@ export const AccordionStory: ComponentStoryObj<typeof Accordion> = {
       contents: [{ id: 0, label: 'test1', text: 'test text' },{ id: 1, label: 'test2', text: 'test text' }]
   },
   render: ({ contents }) => <Accordion contents={contents} />
+}
+
+AccordionStory.play = async({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await expect(await canvas.findByText('test1')).toBeInTheDocument();
 }
