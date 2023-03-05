076d0d761a930f94c40963a5ed53db448e111110
takahashikosuke <takahashikosuke@team-lab.com>
2023-03-05 18:43:42 +0900

    :wrench: 修正

--- src/components/accordion/index.stories.tsx
+++ src/components/accordion/index.stories.tsx
@@ -14,7 +14,7 @@ export default {
 
 export const AccordionStory: ComponentStoryObj<typeof Accordion> = {
   args: {
-      contents: [{ id: 0, label: 'test1', text: 'test text' },{ id: 1, label: 'test2', text: 'test text' }]
+      contents: [{ id: 0, label: 'Accordion1', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum' },{ id: 1, label: 'Accordion1', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum' }]
   },
   render: ({ contents }) => <Accordion contents={contents} />
 }
