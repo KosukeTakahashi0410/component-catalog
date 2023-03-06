361071ca50671af9fceba896e90aaf4750b047ac
takahashikosuke <takahashikosuke@team-lab.com>
2023-03-06 22:15:12 +0900

    :wrench: topを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
-import { Accordion } from '~/components/accordion';
 
 const Home: NextPage = () => {
-  const contents: Array<{ id: number; label: string; text: string }> = [{ id: 0, label: 'test1', text: 'test text' },{ id: 1, label: 'test2', text: 'test text' }]
-
   return (
-    <Accordion contents={contents} />
+    <div>
+      <h1>ここには何もないよ</h1>
+    </div>
   )
 }
 
